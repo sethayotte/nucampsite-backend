51d13ea82723c9f6650ac6ab59d8b04d8dfe736c
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-11-21 11:05:27 -0500

    campsite, partner, promotion router

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const campsiteRouter = express.Router();
+
+campsiteRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send the ${req.params.campsiteId} campsite to you`);
+})
+.post((req, res) => {
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`Updating the campsite: ${req.params.campsiteId} Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting the ${req.params.campsiteId} campsite`);
+});
+
+module.exports = campsiteRouter;
---
+++ routes/partnerRouter.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const partnerRouter = express.Router();
+
+partnerRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the partners to you');
+})
+.post((req, res) => {
+    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res) => {
+    res.end('Deleting all partners');
+});
+
+partnerRouter.route('/:partnerId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send the ${req.params.partnerId} partner to you`);
+})
+.post((req, res) => {
+    res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`Updating the partner: ${req.params.partnerId} Will add the partner: ${req.body.name} with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting the ${req.params.partnerId} partner`);
+});
+
+module.exports = partnerRouter;
